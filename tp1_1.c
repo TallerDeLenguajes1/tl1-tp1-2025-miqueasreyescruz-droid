1e4cc0d23d1969822bb5aa25fd43af55176800eb
MiqueasReyes <miqueasreyescruz@gmail.com>
2026-04-02 23:14:04 -0300

    Se modifico el archivo tp1_1.c con lo que se pide en el ejercicio 4)e)

--- tp1_1.c
+++ tp1_1.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 int main () {
+    int random = 42;
+    int *puntero = &random;
+
     printf("Hola Mundo!\n");
+
+    printf("-Contenido del puntero: %d\n", *puntero);
+    printf("-Direccion de memoria almacenada por el puntero: %p\n", puntero);
+    printf("-Direccion de memoria de la variable: %p\n", &random);
+    printf("-Direccion de memoria del puntero: %p\n", &puntero);
+    printf("-Memoria utilizada por la variable puntero: %lu bytes\n", sizeof(puntero));
+
     return 0;
 }
